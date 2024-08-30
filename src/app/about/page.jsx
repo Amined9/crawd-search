174cd4243e21d289a4c3d1dd047aa9547741b668
Amined9 <mahalhal.amine.sio@gmail.com>
2024-08-30 23:35:05 +0200

    dd

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 export default function About() { 
     return(
         <>
             <Navbar/>
-            <div className="h-screen"></div>
+            <div className=""></div>
+            <Footer/>
         </>
     )
 
