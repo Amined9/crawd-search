e94e7ff7f0a9dd14daea7f69555591b254f827c1
Amined9 <mahalhal.amine.sio@gmail.com>
2024-09-01 23:42:29 +0200

    fin-de

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -12,15 +12,18 @@ export default function Pricing() {
                     <div className="mt-6">
                         <ul className="grid grid-flow-row gap-5 sm:grid-flow-col">
                             <li className="rounded-lg border shadow-md bg-white flex flex-col p-6 h-72 max-w-60">
-                                <div className="text-center text-2xl font-semibold">70 €</div>
+                                <div className="text-center text-2xl font-semibold mb-2">Basic</div>
+                                <div className="text-center text-2xl font-medium border-b border-slate-400">70</div>
                                 <div>sccccccccccccccccccc</div>
                             </li>
                             <li className="rounded-lg border shadow-md bg-white flex flex-col p-6 h-72 max-w-60 ">
-                                <div className="text-center text-2xl font-semibold">150</div>
+                                <div className="text-center text-2xl font-semibold mb-2">Medium</div>
+                                <div className="text-center text-2xl font-medium border-b border-slate-400">100</div>
                                 <div className="text-pretty">scccccccccccccccccccc</div>
                             </li>
                             <li className="rounded-lg border shadow-md bg-white flex flex-col p-6 h-72 max-w-60">
-                                <div className="text-center text-2xl font-semibold">350</div>
+                                <div className="text-center text-2xl font-semibold mb-2">Premium</div>
+                                <div className="text-center text-2xl font-medium border-b border-slate-400">200</div>
                                 <div>sccccccccccccccccccccc</div>
                             </li>
                         </ul>
